2b812e8eb349cb339774ed7ed596e457aeef5f0b
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-07-02 17:59:09 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -10,8 +10,9 @@ it('defines `greeting`', function() {
   expect(greeting).toEqual("Hello, everybody!");
 });
 
-it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
-  expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`);
+it('concatenates strings to greet a special guest in `greetSpecialGuest`',
+  function() {
+  expect(greetSpecialGuest).toEqual"('Hello, ${specialGuest}!`)"});
 });
   
 it('interpolates a string in `conversation`', function() {
